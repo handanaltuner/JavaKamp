a19192c5f38e2fb94d036245dd83554dea2c8b5e
handan_altuner@hotmail.com <handan_altuner@hotmail.com>
2021-05-08 14:01:51 +0300

    nLayerDemo eklendi

---
+++ nLayeredDemo/src/nLayeredDemo/Main.java
@@ -0,0 +1,24 @@
+package nLayeredDemo;
+
+
+
+import nLayeredDemo.business.abstracts.ProductService;
+import nLayeredDemo.business.concretes.ProductManager;
+//import nLayeredDemo.dataAccess.concretes.HibernateProductDao;
+import nLayeredDemo.entities.concretes.Product;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Todo : Sprint IoC ile çözülecek
+		ProductService productService = new ProductManager(new HibernateProductDao()); 
+			
+		Product product= new Product(1,1,"Elma",12,50);
+		productService.add(product);
+		
+		
+		}
+}
+
